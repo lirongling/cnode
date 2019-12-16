1e02ce9d740a6507aef94a06993da3f9e54bfbf0
LAPTOP-FKOQDFD9\李荣亮 <1843306093@.qq.com>
2019-12-16 12:01:17 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,47 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Comm from '../views/Comm'
+
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
+
+
+const routes = [{
+        path: '/home',
+        // 重定向
+        redirect: '/'
+    },
+    {
+        path: "/",
+        component: Comm,
+        children: [{
+            path: '/',
+            name: "home",
+            component: Home,
+            meta: {
+                title: '首页'
+            }
+        }],
+    },
+
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
---
+++ src/views/Comm.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>Header</el-header>
+        <el-container >
+          <el-main width="1050px">Main</el-main>
+          <el-aside width="150px">Aside</el-aside>
+        </el-container>
+      <el-footer>Footer</el-footer>
+    </el-container>
+
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+  mounted() {},
+  watch: {},
+  computed: {}
+};
+</script>
+
+<style scoped lang='scss'>
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+    height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    width: 150px;
+    line-height: 200px;
+  }
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+    display: flex;
+    width: 1335px;
+    margin: 0 auto;
+  }
+</style>
